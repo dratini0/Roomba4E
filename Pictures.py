79dc50a3e80593847ce96ff9763f6d9d51ae09a3
charleslindsay <32023845+charleslindsay@users.noreply.github.com>
2017-09-17 07:04:43 -0400

    first version

---
+++ Pictures.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 06:59:34 2017
+
+@author: Charles
+"""
+########### Python 3.2 #############
+
+##take pic, send to server, return what it thinks it is
+import picamera, time
+camera = picamera.PiCamera()
+camera.rotation = 180
+   
+ for i in range (0,20):
+     
+    camera.capture('/home/pi/image%s.jpg',i)
+    time.sleep(5)
+   
+    
